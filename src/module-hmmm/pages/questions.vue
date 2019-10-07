aa126550d82b1b7de5ba8b363ab7ae4fffc09ea0
c-bd <18334794147@qq.com>
2019-10-07 17:00:03 +0800

    测试更改

--- src/module-hmmm/pages/questions.vue
+++ src/module-hmmm/pages/questions.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="dashboard-container">
-    <div class="app-container">基础题库管理</div>
+    <div class="app-container">
+      <el-row> 
+        <el-col :span='6'>
+          学科：
+          <el-select name=""></el-select>
+        </el-col>
+        <el-col :span='6'>
+          难度：
+          <el-select></el-select>
+        </el-col>
+        <el-col :span='6'>
+          试题类型：
+          <el-select></el-select>
+        </el-col>
+      </el-row>
+      <el-button type='success'>提交</el-button>
+    </div>
   </div>
 </template>
 
