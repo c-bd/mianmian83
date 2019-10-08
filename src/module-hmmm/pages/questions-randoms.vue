d1372426417e3c84d9a68e3ce220499ee27b53d4
lx-ghb <3524317373@qq.com>
2019-10-08 08:27:35 +0800

    组题列表，id大数字

--- src/module-hmmm/pages/questions-randoms.vue
+++ src/module-hmmm/pages/questions-randoms.vue
@@ -1,17 +1,96 @@
 <template>
   <div class="dashboard-container">
-    <div class="app-container">组题列表</div>
+    <div class="app-container">
+      <el-table
+      :data="randomslist"
+      border
+      style="width: 100%">
+    <el-table-column
+      prop="id"
+      label="序号"
+      width="80">
+    </el-table-column>
+    <el-table-column
+      prop="addTime"
+      label="组题时间"
+      width="160">
+    </el-table-column>
+    <el-table-column
+      prop="userName"
+      label="用户名"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="questionIDs"
+      label="试题ID">
+    </el-table-column>
+    <el-table-column
+      prop="progressOfAnswer"
+      label="答题进度"
+      width="100">
+    </el-table-column>
+    <el-table-column
+      prop="accuracyRate"
+      label="正确率">
+    </el-table-column>
+    <el-table-column
+      prop="totalSeconds"
+      label="答题总耗时(秒)">
+    </el-table-column>
+    <el-table-column
+      prop="questionType"
+      label="组题类型/详情">
+    </el-table-column>
+    <el-table-column
+      label="操作">
+      <template>
+        <el-button type="text" @click="deletelist(id)">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+    </div>
   </div>
 </template>
 
 <script>
+// 把api数据相关接口方法导入
+import { randoms, removeRandoms } from '@/api/hmmm/questions'
 export default {
   name: 'QuestionsRandoms',
   data() {
-    return {}
+    return {
+      randomslist: []
+    }
+  },
+  methods: {
+    // 获取组题列表数据
+    async getrandomslist() {
+      var rst = await randoms()
+      console.log(rst) 
+      this.randomslist = rst.data.items
+      console.log(rst.data.items)
+    },
+    deletelist(id) {
+      this.$confirm('此操作将永久删除该条数据, 是否继续?', '提示').then(async () => {
+        await deletelist({id: id}).then(response => {
+          // 响应回来的数据
+          this.$message.success('成功删除该条数据')
+          // 后台数据删除后，将页面上的数据也删除
+          this.randomslist.splice(id, 1)
+        }).catch(response => {
+          this.$message.error('删除失败')
+        }).catch(() => {
+          this.$message.info('已取消删除')
+        })
+      })
+    }
+  },
+  created() {
+    this.getrandomslist()
   }
 }
 </script>
 
 <style scoped>
+  
 </style>
--- src/module-hmmm/pages/questions.vue
+++ src/module-hmmm/pages/questions.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="dashboard-container">
-    <div class="app-container">基础题库管理</div>
+    <div class="app-container">
+      <el-row type="flex">
+        <el-button type="info">新增试题</el-button>
+        <el-button type="info">批量导入</el-button>
+      </el-row>
+      <el-row>
+        <el-col>
+         活动区域：
+            <el-select placeholder="请选择">
+            </el-select>
+        </el-col>
+        <el-col>
+         难度：
+            <el-select placeholder="请选择">
+            </el-select>
+        </el-col>
+        <el-col>
+         试题类型：
+            <el-select placeholder="请选择">
+            </el-select>
+        </el-col>
+        <el-col>
+         标签：
+            <el-select placeholder="请选择">
+            </el-select>
+        </el-col>
+        <el-col>
+         城市：
+            <el-select placeholder="请选择">
+            </el-select>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
